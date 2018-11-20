b4bf346cd2729478b04e97e36b14df7f02436226
kahanor <karl.rielaender@gmail.com>
2018-11-20 14:32:33 +0100

    created basic data input pipeline

--- titanic_data_import.py
+++ titanic_data_import.py
@@ -1,8 +1,25 @@
 import tensorflow as tf
 
 NUM_EPOCHS = 5
+BATCH_SIZE = 100
 
-dataset = tf.data.TextLineDataset(['train.csv'])
 
-dataset = dataset.shuffle(100000)
-dataset = dataset.repeat(NUM_EPOCHS)
+def nn_func(dataset):
+    X = dataset['Pclass']
+    Y = tf.layers.dense(X, tf.int32, activation='softmax')
+    return Y
+
+
+def _encode(features, labels):
+    return features, labels
+
+
+tf.enable_eager_execution()
+
+dataset = tf.data.experimental.make_csv_dataset(
+    'train.csv', BATCH_SIZE, label_name='Survived',
+    select_columns=['Pclass', 'Sex', 'Age', 'Survived'], num_epochs=NUM_EPOCHS
+)
+
+for batch in dataset:
+    print(batch)
